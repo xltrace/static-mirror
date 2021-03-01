9cb499c0fab7e05dd7721f7cfaabb1ac5c14a894
Sent G. Wierda <sentwierda@gmail.com>
2021-03-01 13:08:08 +0100

    module wiki fixed sitemap-wiki.xml

--- module.wiki.php
+++ module.wiki.php
@@ -8,20 +8,22 @@ class wiki extends \XLtrace\Hades\module {
     if(!is_dir($this->root)){ return FALSE; }
     $str = FALSE;
     $el = $this->extentions();
+    $ext = FALSE; if(preg_match('#\.([a-z0-9]+)$#', $for, $buff)){ $ext = $buff[1]; }
     switch(strtolower($for)){
       case 'toc': $str = $this->toc(); break;
       case 'sitemap.xsl': $str = \XLtrace\Hades\module_get('sitemap','sitemap.xsl'); $this->mode = 'application/xsl'; break;
+      case 'sitemap-wiki.xml': case 'sitemap-wiki.json':
+        $map = $this->mapper_sitemap_json();
+        //if(in_array($ext, array('xml','json','xsl','html')) && $for == substr($this->get_sitemap_URI(TRUE), 0, -3).$ext){
+          $str = \XLtrace\Hades\module_get('sitemap','sitemap.'.$ext,$map); $this->mode = 'application/'.$ext;
+        //}
+        break;
       default:
-        $ext = FALSE; if(preg_match('#\.([a-z0-9]+)$#', $for, $buff)){ $ext = $buff[1]; }
-        if(in_array($ext, array('xml','json','xsl','html')) && $for == substr($this->get_sitemap_URI(TRUE), 0, -3).$ext){
-          $str = \XLtrace\Hades\module_get('sitemap','sitemap.'.$ext,$this->mapper_sitemap_json()); $this->mode = 'application/'.$ext;
-        } else {
-          /*limited with ext*/ if(preg_match('#\.('.implode('|', $el).')$#', $for, $buffer)){ $el = array($buffer[1]); $for = substr($for, 0, -1*(strlen($buffer[1])+1)); }
-          foreach($el as $ext){
-            $f = $this->root.$for.'.'.$ext;
-            if(file_exists($f)){
-              $str = file_get_contents($f); $el = array($ext); $this->file['src'] = $f; $this->file['ext'] = $ext;
-            }
+        /*limited with ext*/ if(preg_match('#\.('.implode('|', $el).')$#', $for, $buffer)){ $el = array($buffer[1]); $for = substr($for, 0, -1*(strlen($buffer[1])+1)); }
+        foreach($el as $ext){
+          $f = $this->root.$for.'.'.$ext;
+          if(file_exists($f)){
+            $str = file_get_contents($f); $el = array($ext); $this->file['src'] = $f; $this->file['ext'] = $ext;
           }
         }
     }
